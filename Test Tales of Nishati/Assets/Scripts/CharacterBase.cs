e83a5d978a5e69cdaba5fc25b564f345c8eb877b
W. Souza Carvalho <w.souza.carvalho@avanade.com>
2019-07-31 17:17:56 -0300

    initial import

---
+++ Test Tales of Nishati/Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterBase : MonoBehaviour
+{
+    //Basics atributes
+    public int currentLevel;
+    public float startLife;
+    public float startMana;
+    public int strength;
+    public int agility;
+    public int baseDefence;
+    public int baseAttack;
+    
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        
+    }
+}
---
+++ Test Tales of Nishati/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TypeCharacter
+{
+    Warrior = 0,
+    Warlock = 1,
+    Archer = 2
+}
+
+public class PlayerBehavior : CharacterBase
+{
+    private TypeCharacter type;
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        base.Start();
+        currentLevel = PlayerStatsController.GetCurrentLevel();
+        type = PlayerStatsController.GetTypeCharacter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
